2a41c22aab9668f76bcb58d029b933200d2e35c0
valetzx <45054400+valetzx@users.noreply.github.com>
2022-05-10 10:59:34 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,4 +8,6 @@ create -h 127.0.0.1 -p 5432 user admin with password 'admin';
 createdb -h 127.0.0.1 -p 5432 kanboard
 git clone https://github.com/kanboard/kanboard
 fi
+
+cp config.php kanboard
 php -S 0.0.0.0:8000 -t kanboard
