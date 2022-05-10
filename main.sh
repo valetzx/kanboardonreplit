c6e416359adc504b1a46e3da2c09f4ef2996cd9e
valetzx <45054400+valetzx@users.noreply.github.com>
2022-05-10 10:49:17 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,11 @@
+sh startpg.sh
+pg_ctl stop
+pg_ctl -l ./postgresql.log start
+
+if [ ! -f "kanboard" ];then
+#PG创建数据库：typecho
+create -h 127.0.0.1 -p 5432 user admin with password 'admin';
+createdb -h 127.0.0.1 -p 5432 kanboard
+git clone https://github.com/kanboard/kanboard
+fi
+php -S 0.0.0.0:8000 -t kanboard
